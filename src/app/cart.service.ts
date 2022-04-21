cdc7f96e95f9166a545bb339526f3b2040a8f667
Akira Taguchi <akirahattivatti@gmail.com>
2022-04-21 16:02:23 +0300

    Deploy to GitHub Pages

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -13,7 +13,7 @@ export class CartService {
 
   getShippingPrices() {
     return this.http.get<{ type: string, price: number }[]>
-      ('/assets/shipping.json')
+      ('assets/shipping.json')
   }
 
   addToCart(product: Product) {
